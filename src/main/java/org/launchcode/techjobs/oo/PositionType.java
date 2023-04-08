fcfd3df6cd67aea8fd24f9c1828db89d1dcd4983
Paul <greatdogsite@gmail.com>
2023-04-08 11:57:18 -0500

    2) Supporting Classes Completed

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -28,6 +28,7 @@ public class PositionType {
     //  -done their id fields match.
 
     @Override
+    
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
